b057c3caf91648f1960f791c38b64e0f6e49a190
Wszolek Jakub - jwszol <jwszol@acxiom.com>
2017-12-01 12:28:40 +0100

    time epoch added

--- src/main/scala/com/smartstorm/utils/CassandraUtils.scala
+++ src/main/scala/com/smartstorm/utils/CassandraUtils.scala
@@ -25,6 +25,7 @@ object CassandraUtils {
   def insertSensorData(message: Message, table: String, session: Session): Unit = {
     val format = new SimpleDateFormat("d-M-y hh:mm:ss")
     val timestamp = format.format(Calendar.getInstance().getTime())
+    val timeEpoch = Calendar.getInstance().getTimeInMillis()/1000L
 
     val insertQuery = QB.insertInto(table)
       .value("sensorid", message.sensorId)
@@ -32,6 +33,7 @@ object CassandraUtils {
       .value("sensordesc", "test desc")
       .value("value",message.value)
       .value("created", timestamp)
+      .value("created_epoch",timeEpoch)
       .using(QB.ttl(86400))
 
     session.execute(insertQuery)
